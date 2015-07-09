25c87dc34aa57be257a364b68ed380cdaa3b2eb0
Erik Bryn <erik.bryn@gmail.com>
2015-07-09 15:51:01 -0500

    Compose controller walkthrough / alternative approach to use a component for route

---
+++ app/controllers/messages/compose.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  body: "a default value",
+
+  actions: {
+    send() {
+      this.set('body', 'you sent me!')
+    }
+  }
+});
