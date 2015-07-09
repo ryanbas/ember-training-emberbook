3ac6ede5930ce5855d9c9a4a81989ae5690201e8
Ryan Bas <krados@gmail.com>
2015-07-09 15:01:11 -0500

    markdown-preview component

---
+++ app/components/markdown-preview.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  preview: Ember.computed('text', function() {
+    var text = this.get('text') || '';
+    return markdown.toHTML(text).htmlSafe();
+  })
+});
